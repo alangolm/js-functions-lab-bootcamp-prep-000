117248fc10496834e57420b16c0aa343a9a947c3
Alan Golman <golman.alan@gmail.com>
2018-04-12 22:37:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function happyHolidays() {
 }
 function happyHolidaysTo(name) {
   return ("Happy holidays, you!");
+<<<<<<< HEAD
 }
 function happyHolidayTo(holiday, name) {
   return ("Happy Independence Day, you!");
@@ -10,3 +11,7 @@ function happyHolidayTo(holiday, name) {
 function holidayCountdown(holiday, days) {
   return ("It's 20 days until Mother's Day!");
 }
+=======
+  else if 
+}
+>>>>>>> c8598f1fc56dd638b19617d5553f60ee072a193d
